23310b4af1da6af772b49258aa1d0379e10c42d9
Jacob Groundwater <jacob@newrelic.com>
2013-12-05 13:05:05 -0800

    cleanup demo

--- demo.js
+++ demo.js
@@ -3,33 +3,28 @@
 // long traces are enabled by default
 var lt = require('./index.js');
 
-var util = require('util');
-var fs   = require('fs');
-
 //----- Demo -----//
-// throw new Error();
-
 var k = 0;
 setImmediate(function A() {
   process.nextTick(function SHOULD_NOT_APPEAR_IN_TRACE() {
-    process.nextTick(function SHOULD_NOT_APPEAR_IN_TRACE() {
-      return 1;
-    });
-    return 2;
+    setTimeout(function SHOULD_NOT_APPEAR_IN_TRACE() {
+      return;
+    }, 500);
+    return;
   });
   setTimeout(function B() {
     process.nextTick(function SHOULD_NOT_APPEAR_IN_TRACE() {
-      return 3;
+      return;
     });
     process.nextTick(function C() {
       (function SHOULD_NOT_APPEAR_IN_TRACE(){
-        return 4;
+        return;
       })();
       throw new Error('k=' + k);
     });
   },100);
   setTimeout(function SHOULD_NOT_APPEAR_IN_TRACE() {
-    return 6;
     console.log("SHOULD NOT APPEAR IN TRACE");
+    return;
   }, 200);
 });
